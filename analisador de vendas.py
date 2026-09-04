525291d69639a2d105d0c2c23d498dc0a3e51969
jonasdev447 <jns204c@gmail.com>
2026-09-04 04:13:55 -0300

    Add files via upload

---
+++ analisador de vendas.py
@@ -0,0 +1,21 @@
+vendas = [120, 250, 90, 430, 180, 300, 75]
+
+print()
+
+print("total de vendas: ", len(vendas))
+
+print()
+
+print("valor total de vendas: ", sum(vendas))
+
+print()
+
+print("menor venda:", min(vendas))
+
+print()
+
+print("maior venda:", max(vendas))
+
+
+media = sum(vendas) / len(vendas)
+print("Venda média:", round(media, 2))
